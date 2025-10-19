90219d7a4bedfebaa9efc907b2bffcec69ae76da
Naveed03010408761 <akmal030104087@gmail.com>
2025-10-19 08:54:46 +0500

    Fixing the further bugs

--- blogapp/src/app/layout.tsx
+++ blogapp/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import AuthProvider from "./providers/AuthProvider"; // ✅ Add this
+import AuthProvider from "./providers/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
